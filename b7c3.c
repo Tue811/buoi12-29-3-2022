f737035a24e437b0f5487b8d980f1a9e0d627792
Nguyễn Thị Bích Tuệ <95497729+Tue811@users.noreply.github.com>
2022-03-31 11:25:28 +0700

    update

---
+++ b7c3.c
@@ -0,0 +1,114 @@
+#include<stdio.h>
+#include<conio.h>
+#include<stdlib.h>
+
+struct Node{
+    int Inf;
+    struct Node *Next;
+};
+typedef struct Node LIST;
+
+struct oddlist {
+	LIST* pHead;
+	LIST* pTail;
+};
+typedef struct oddlist OddList;
+
+LIST * getNode (int a){
+    LIST* l = (LIST*)malloc(sizeof(LIST));
+	if(l == NULL) {
+		printf("Loi cap phat bo nho");
+		return NULL;
+	}
+	l->Inf = a;
+	l->Next = NULL;
+	return l;
+}
+
+void init(OddList *o)
+{
+    o->pHead = o->pTail = NULL;
+}
+
+void addTail(OddList *o,LIST *l)
+{
+    if(o->pHead == NULL)
+    {
+        o->pHead = o->pTail = l;
+    }
+    else {
+        o->pTail->Next=l;
+        o->pTail=l;
+    }
+}
+
+int getInt()
+{
+    int b;
+    scanf("%d",&b);
+    return b;
+}
+
+void input (OddList *o, int *n)
+{
+    int a;
+    printf("nhap so luong phan tu: ");
+    scanf("%d",n);
+    init(o);
+    for(int i=1;i<=*n;i++)
+    {
+        LIST *l = getNode(getInt());
+        addTail(o,l);
+    }
+}
+
+
+void output(OddList o)
+{
+    for(LIST *l = o.pHead; l!=NULL; l=l->Next)
+    {
+
+            printf("%d ",l->Inf);
+    }
+}
+void removeDuplicates(OddList* o)
+{
+    if (o->pHead == NULL) {
+        return;
+    }
+
+    LIST *p = o->pHead;
+
+    while (p->Next != NULL)
+    {
+        if (p->Inf == p->Next->Inf)
+        {
+            LIST *nextNext = p->Next->Next;
+            free(p->Next);
+            p->Next = nextNext;
+        }
+        else {
+            p = p->Next;    
+        }
+    }
+}
+
+void deleteNode(OddList *o) {
+	LIST *l;
+	while(o->pHead != NULL) {
+		l = o->pHead;
+		o->pHead = o->pHead->Next;
+		free(l);
+	}
+}
+
+int main()
+{
+    OddList o;
+    int n;
+    input(&o,&n);
+    removeDuplicates(&o);
+    output(o);
+    deleteNode(&o);
+    return 0;
+}
